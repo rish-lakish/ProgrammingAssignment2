dd500e6af6238702361f04f4fd7fe08d422e54de
rish-lakish <rish-lakish@users.noreply.github.com>
2015-04-19 15:13:07 +0300

    Added makeCacheMatrix and cacheSolve function implementations.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## cachematrix.R - Code to cache/restore an inverse of a matrix.
+##
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix - create and return a matrix object that can cache
+## it's inverse. Internally, this object is a list of 'member'
+## functions that can set and retreive the value of the matrix, and
+## it's inverse.
+makeCacheMatrix <- function(x=matrix()) {
 
+    ## The 'cached-inverse' member.
+    cached.inv <- NULL
+
+
+    ## Set the value of the matrix wrapped by this object.
+    set.matrix <- function(new.matrix) {
+        x <<- new.matrix
+        cached.inv <<- NULL
+    }
+
+
+    ## Get matrix wrapped by this object.
+    get.matrix <- function() {
+        x
+    }
+
+
+    ## Get the inverse of the matrix wrapped by this object, computing
+    ## it if necessary. As per the instructions in the assignment, we
+    ## assume the matrix is always invertible. An error will be trown
+    ## if it is singular.
+    get.inverse <- function() {
+        if (is.null(cached.inv)){
+            ## No cached value. Compute the inverse.
+            cached.inv <<- solve(x)
+        }
+        cached.inv
+    }
+
+
+    list(set.matrix=set.matrix,
+         get.matrix=get.matrix,
+         get.inverse=get.inverse)
 }
 
 
-## Write a short comment describing this function
 
+##
+## Return the inverse of a matrix object created using
+## 'makeCacheMatrix', using the cache if possible to avoid unneeded
+## computations
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Functionality is already implemented by the get.inverse method of
+    ## the matrix object, so just forward the request to it.
+    x$get.inverse()
 }
